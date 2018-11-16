ff915debdddfdd77cff350e2e8b16004499eb2b8
Glutamatt <glutamatt@live.fr>
2018-11-16 02:08:04 +0100

    IT WORKS

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
 	"os"
@@ -15,43 +15,27 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func readLine(reader *bufio.Reader) (string, error) {
-	isPrefix := true
-	line := bytes.NewBuffer([]byte{})
-	var linePart []byte
-	var err error
-	for isPrefix {
-		linePart, isPrefix, err = reader.ReadLine()
-		if err != nil {
-			return "", fmt.Errorf("Error on reader.ReadLine: %v", err)
-		}
-		n, err := line.Write(linePart)
-		if err != nil {
-			return "", fmt.Errorf("Error on line.Write: %v", err)
-		}
-		if n != len(linePart) {
-			return "", fmt.Errorf("Error on line.Write written bytes len: %v", err)
-		}
-	}
-
-	return line.String(), nil
-}
-
 func main() {
 	u := url.URL{Scheme: "ws", Host: "localhost:3000", Path: "/_ws"}
 	log.Printf("connecting to %s", u.String())
-
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-
 	log.Printf("connected to %s", u.String())
 
 	done := make(chan struct{})
 	interrupt := make(chan os.Signal, 1)
+	responses := make(chan []byte)
 	signal.Notify(interrupt, os.Interrupt)
+
+	back := "http://192.168.0.18:3000"
+	backendURL, err := url.ParseRequestURI(back)
+	if err != nil {
+		log.Fatal("url.ParseRequestURI %s err : %v", back, err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(backendURL)
 
 	go func() {
 		defer close(done)
@@ -61,85 +45,27 @@ func main() {
 				log.Println("read:", err)
 				return
 			}
+			if messageType == websocket.CloseMessage {
+				log.Println("Close message from the server", string(message))
+				return
+			}
 			if messageType == websocket.TextMessage {
-				reader := bufio.NewReader(bytes.NewReader(message))
-				hash, err := readLine(reader)
-				if err != nil {
-					log.Printf("HASH: %s \n err: %v\n", hash, err)
-					return
-				}
-				/*
-					MESSAGE, _ := ioutil.ReadAll(reader)
-					log.Println("MESSAGE ", string(MESSAGE))
-				*/
-
-				req, err := http.ReadRequest(reader)
-				if err != nil {
-					log.Println("http.ReadRequest ERR :", err)
-					return
-				}
-				url := "http://grafana.deez.re" + req.URL.String()
-
-				/*
-					body, _ := ioutil.ReadAll(req.Body)
-					log.Println("BODY ", string(body))
-				*/
-
-				forgedReq, err := http.NewRequest(req.Method, url, req.Body)
-				if err != nil {
-					log.Println("http.NewRequest ERR :", err)
-					return
-				}
-				for k, v := range req.Header {
-					if k != "Host" {
-						forgedReq.Header[k] = v
-					}
-				}
-
-				/*
-					body, _ := ioutil.ReadAll(forgedReq.Body)
-					log.Println("BODY ", string(body))
-				*/
-
-				debug, _ := httputil.DumpRequest(forgedReq, true)
-				log.Println("debug ", string(debug))
-
-				res, err := http.DefaultClient.Do(forgedReq)
-				if err != nil {
-					log.Println("http.DefaultClient.Do ERR :", err)
-					return
-				}
-				dump, err := httputil.DumpResponse(res, true)
-				if err != nil {
-					log.Println("httputil.DumpResponse ERR :", err)
-					return
-				}
-				if err := c.WriteMessage(websocket.TextMessage, append([]byte(hash+"\n"), dump...)); err != nil {
-					log.Println("c.WriteMessage ERR :", err)
-					return
-				}
+				go handleRequest(message, responses, proxy)
 			}
 		}
 	}()
 
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
 	for {
 		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
-			if err != nil {
-				log.Println("write:", err)
+		case resp := <-responses:
+			if err := c.WriteMessage(websocket.TextMessage, resp); err != nil {
+				log.Println("c.WriteMessage ERR :", err)
 				return
 			}
+		case <-done:
+			return
 		case <-interrupt:
 			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
@@ -152,4 +78,29 @@ func main() {
 			return
 		}
 	}
+}
+
+func handleRequest(message []byte, responses chan []byte, proxy *httputil.ReverseProxy) {
+	reader := bufio.NewReader(bytes.NewReader(message))
+	hash, err := reader.ReadString('\n')
+	if err != nil {
+		log.Printf("HASH: %s \n err: %v\n", hash, err)
+		return
+	}
+	req, err := http.ReadRequest(reader)
+	if err != nil {
+		log.Println("http.ReadRequest ERR :", err)
+		return
+	}
+
+	rw := httptest.NewRecorder()
+	proxy.ServeHTTP(rw, req)
+	resp, err := httputil.DumpResponse(rw.Result(), true)
+
+	if err != nil {
+		log.Println("httputil.DumpResponse ERR :", err)
+		return
+	}
+
+	responses <- append([]byte(hash), resp...)
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"crypto/sha1"
 	"fmt"
 	"io"
@@ -9,6 +11,8 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -16,13 +20,21 @@ import (
 func handler(calls chan<- HttpCall) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ch := make(chan *http.Response)
+		defer close(ch)
+		timer := time.NewTimer(1 * time.Second)
+		defer timer.Stop()
 		select {
 		case calls <- HttpCall{req: r, resp: ch}:
-			fmt.Fprintf(w, "Req pushed to httpCalls chan")
 			resp := <-ch
+			for k, vv := range resp.Header {
+				for _, v := range vv {
+					w.Header().Add(k, v)
+				}
+			}
+			w.WriteHeader(resp.StatusCode)
 			io.Copy(w, resp.Body)
-		default:
-			fmt.Fprintf(w, "ERROR: Unable to push Req to httpCalls chan")
+		case <-timer.C:
+			fmt.Fprintf(w, "ERROR timeout: Unable to push Req to httpCalls chan")
 		}
 	}
 }
@@ -54,6 +66,8 @@ func main() {
 
 		defer conn.Close()
 
+		disp := &dispatcher{pipes: map[string]chan<- *http.Response{}}
+
 		go func() {
 			for {
 				messageType, message, err := conn.ReadMessage()
@@ -62,7 +76,19 @@ func main() {
 					return
 				}
 				if messageType == websocket.TextMessage {
-					log.Println(string(message))
+					log.Println("websocket.TextMessage incoming", string(message[:500]))
+					reader := bufio.NewReader(bytes.NewReader(message))
+					hash, err := reader.ReadString('\n')
+					if err != nil {
+						log.Printf("HASH: %s \n err: %v\n", hash, err)
+						continue
+					}
+					resp, err := http.ReadResponse(reader, nil)
+					if err != nil {
+						log.Printf("http.ReadResponse err: %v", err)
+						continue
+					}
+					go disp.Serve(strings.TrimRight(hash, "\n"), resp)
 				}
 			}
 		}()
@@ -77,13 +103,14 @@ func main() {
 					httpCall.resp <- errorResponse(fmt.Errorf("httputil.DumpRequest error: %v", err))
 					continue
 				}
-				data := append([]byte(fmt.Sprintf("%x\n", sha1.Sum(req))), req...)
+				hash := fmt.Sprintf("%x", sha1.Sum(req))
+				data := append([]byte(fmt.Sprintf("%s\n", hash)), req...)
 				if err = conn.WriteMessage(websocket.TextMessage, data); err != nil {
 					httpCall.resp <- errorResponse(fmt.Errorf("conn.WriteMessage error (closing backend websocket connection): %v", err))
 					return
 				}
-				log.Println("conn.WriteMessage OK")
-				httpCall.resp <- errorResponse(fmt.Errorf("Yeah the req has been pushed on WS"))
+				log.Println("conn.WriteMessage OK", "hash", hash)
+				go disp.Handle(hash, httpCall.resp)
 			}
 		}
 	})
@@ -91,4 +118,32 @@ func main() {
 	http.HandleFunc("/", handler(httpCalls))
 	log.Println("Let's go !")
 	log.Fatal(http.ListenAndServe(":3001", nil))
+}
+
+type dispatcher struct {
+	lock  sync.Mutex
+	pipes map[string]chan<- *http.Response
+}
+
+func (d *dispatcher) Handle(hash string, resp chan<- *http.Response) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	log.Println("Handle", hash)
+	d.pipes[hash] = resp
+}
+
+func (d *dispatcher) Serve(hash string, resp *http.Response) {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	log.Println("Serve", hash)
+	if call, exist := d.pipes[hash]; exist {
+		log.Println("Serve", hash, "exist")
+		call <- resp
+		delete(d.pipes, hash)
+		log.Println("Serve", hash, "pushed")
+		return
+	}
+	log.Println("Serve", hash, "doesn't exist !!!")
 }
